f8a38d9d1b5844e6fd53be5964be0ca21bb790db
Ungsik yun <ungsik.yun@gmail.com>
2015-06-18 16:45:24 +0900

    merge js files into local_js.js

---
+++ js/local_js.js
@@ -0,0 +1,26 @@
+/**
+ * Created by user on 15-6-12.
+ */
+jQuery(document).ready(function($) {
+    var data = {
+        'action': 'spri_ajax',
+        'whatever': ajax_object.we_value      // We pass php values differently!
+    };
+    // We can also pass the url value separately from ajaxurl for front end AJAX implementations
+    jQuery.post(ajax_object.ajax_url, data, function(response) {
+        alert('Got this from the server: ' + response);
+    });
+});
+
+/**
+ * Created by user on 15-6-12.
+ */
+jQuery(document).ready(function($) {
+    jQuery('#show_add').click(
+        function () {
+            jQuery(".add_new_chart").toggleClass('display-none');
+            console.log("clicked!");
+        });
+});
+
+jQuery("#")
